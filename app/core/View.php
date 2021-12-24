791379559d94b729a9f60ae8b86407405d64e114
Mihhail K <mihhail.kushnirjov@motherlink.io>
2021-12-24 23:46:17 +0200

    update core View class

--- app/core/View.php
+++ app/core/View.php
@@ -4,25 +4,30 @@ namespace core;
 
 use \React\Http\Message\Response;
 use \core\Env;
+use \Twig\TwigFunction;
+use \Twig\Loader\FilesystemLoader;
+use \Twig\Environment;
 use RuntimeException;
 
 class View
 {
     public static function render(string $page, array $data = [], array $headers = [], int $statusCode = 200): Response
     {
-        $loader = new \Twig\Loader\FilesystemLoader(Env::get('VIEW_PATH') ?: []);
+        $loader = new FilesystemLoader(Env::get('VIEW_PATH') ?: []);
 
         if (!$templateCache = Env::get('TWIG_CACHE')) {
             throw new RuntimeException('Twig cache path empty.');
         }
 
-        $twig = new \Twig\Environment($loader, [
+        $twig = new Environment($loader, [
             // TODO: Disable cache on development for hot reload. Need fix.
             // 'cache' => $templateCache,
             'debug' => true,
         ]);
 
-        $includeAssests = new \Twig\TwigFunction('webpack_inc', function (string $filename) {
+        $twig->addGlobal('URL', Env::get('URL'));
+
+        $includeAssests = new TwigFunction('webpack_inc', function (string $filename) {
             if ($manifestJson = file_get_contents('/home/crypto/Desktop/www/html/public/manifest.json')) {
                 $decodedJson = json_decode($manifestJson);
 
@@ -33,7 +38,7 @@ class View
         });
 
         $twig->addFunction($includeAssests);
-        
+
         return new Response(
             $statusCode,
             $headers,
@@ -47,6 +52,14 @@ class View
             $statusCode,
             $headers,
             $json,
+        );
+    }
+
+    public static function redirect(int $statusCode, array $headers): Response
+    {   
+        return new Response(
+            $statusCode,
+            $headers,
         );
     }
 }
